52f2778fd84348edad1f4841e8db4a3a2ada937f
Oscardo <oscardo2000@gmail.com>
2024-01-02 20:10:14 -0500

    nuevos archivos

---
+++ frontend-tareas/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <tarea></tarea>
+</div>
+</template>
+
+<script>
+import tarea from './components/tareaVue.vue';
+
+export default {
+  name: 'App',
+  components: {
+    tarea
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend-tareas/src/components/tareaVue.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1 class="display-4 text-center">LISTADO DE TAREAS</h1>
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <div class="input-group">
+                            <input type="text" class="form-control form-control-lg" placeholder="Agregar Tarea">
+                            <div class="input-group-append">
+                                <button class="btn btn-success btn-lg">
+                                    Agregar
+                                </button>
+                            </div>
+                        </div>
+                        <br>
+                            <ul class="list-group">
+                                <li class="list-group-item d-flex justify-content-between">
+                                    <span class="cursor">
+                                        <i class="fa-regular fa-circle"></i>
+                                    </span>
+                                    Hacer la tarea!!!
+                                    <span class="text-danger cursor">
+                                        <i class="fa-solid fa-trash-can"></i>
+                                    </span>
+                                </li>
+                            </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+    
+<script lang="ts" setup>
+
+</script>
+<style scoped>
+.cursor{
+    cursor: pointer;
+}
+</style>
